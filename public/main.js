c262bee0c812ab3d2e701ef68483e7abc67e289c
ThienBui1801 <buiquythien1801@gmail.com>
2022-03-19 20:06:54 +0700

    add event

--- public/main.js
+++ public/main.js
@@ -25,5 +25,50 @@ btnExplore.onclick = () => {
   <p>Date: ${date}</p>
   <p>Welcome!</p>
   `
-
 }
+
+// Login - Register
+const btnLogin = document.querySelector('#btn-login');
+const btnRegister = document.querySelector('#btn-register');
+const modalLogin = document.querySelector('#login');
+const login = document.querySelector('#login-container');
+const modalRegister = document.querySelector('#register');
+const register = document.querySelector('#register-container');
+const closeLogin = document.querySelector('#btn-close-in');
+const closeRegister = document.querySelector('#btn-close-reg');
+
+function hideModalLogin() {
+  modalLogin.classList.add('invisible')
+}
+
+btnLogin.addEventListener('click', () => {
+  modalLogin.classList.toggle('invisible')
+})
+
+closeLogin.addEventListener('click', hideModalLogin);
+modalLogin.addEventListener('click', hideModalLogin);
+login.addEventListener('click', (e) => {
+  e.stopPropagation()
+})
+
+function hideModalRegister() {
+  modalRegister.classList.add('invisible')
+}
+
+btnRegister.addEventListener('click', () => {
+  modalRegister.classList.toggle('invisible')
+})
+
+closeRegister.addEventListener('click', hideModalRegister);
+modalRegister.addEventListener('click', hideModalRegister);
+register.addEventListener('click', (e) => {
+  e.stopPropagation()
+})
+
+// Information collapsible
+const btnInfo = document.querySelector('#btn-info')
+const infoDesc = document.querySelector('.information-desc');
+
+btnInfo.addEventListener('click', () => {
+  infoDesc.classList.toggle('invisible')
+})
